d1a3cc4daf5e9fc66ca9b96221160319c2e81b08
AurelijusK <42798425+AurelijusK@users.noreply.github.com>
2018-10-23 22:33:16 +0300

    2018-10-23

---
+++ stiliai/porto/index.php
@@ -0,0 +1,176 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="stiliai/<?php echo input(strip_tags($conf['Stilius'])); ?>/style.css" />
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
+    <meta charset="utf-8">
+    <title>Portfolio</title>
+</head>
+<body>
+
+
+<!--Navbar-->
+<nav class="navbar navbar-dark red lighten-1 mb-4">
+
+    <!-- Navbar brand -->
+    <a class="navbar-brand" href="#">Start Bootstrap</a>
+
+    <!-- Collapse button -->
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent23" aria-controls="navbarSupportedContent23"
+        aria-expanded="false" aria-label="Toggle navigation"><div class="animated-icon3"><span></span><span></span><span></span><span></span></div></button>
+
+    <!-- Collapsible content -->
+    <div class="collapse navbar-collapse" id="navbarSupportedContent23">
+
+        <!-- Links -->
+        <ul class="navbar-nav mr-auto">
+                <?php
+
+                $limit = 6; //Kiek nuorodų rodome
+
+                $menuSql  = mysql_query1( "SELECT * FROM `" . LENTELES_PRIESAGA . "page` WHERE `parent` = 0 AND `show` = 'Y' AND `lang` = " . escape(lang()) . " ORDER BY `place` ASC LIMIT " . $limit );
+
+                ?>
+
+                <?php foreach ($menuSql as $menuRow) { ?>
+
+                <?php if (teises( $menuRow['teises'], $_SESSION[SLAPTAS]['level'])){ ?>
+
+            <li class="nav-item active">
+                <a class="nav-link" href="<?php echo url('?id,' . (int)$menuRow['id']); ?>"><?php echo input($menuRow['pavadinimas']); ?></a>
+            </li>
+            
+           
+				<?php } ?>
+
+                <?php } ?>
+        </ul>
+        <!-- Links -->
+
+    </div>
+    <!-- Collapsible content -->
+
+</nav>
+<!--/.Navbar-->
+
+
+
+<section  class='yellow'>
+    <div class="container-fluid flex">
+        <div class='title'>
+            <h1 class='helo'><B>Stylish Portfolio</B></h1>
+            <h2><I>A Free Bootstrap Theme be Start Bootstrap</I></h2>
+            <button class='button btn-blue'>Find Out More</button>
+        </div>
+    </div>
+</section>
+
+<section class='white'>
+    <div class="container-fluid">
+        <div class='title'>           
+            <h2>Stylish Portfolio is the perfect theme for your next project!</h2>
+            <p>This theme features a flexable, UX frendly sidebar menu and stock photos from our frinds at Unspalsh!</p>
+            <button class='button btn-grey'>What We Offer</button>
+        </div>
+    </div>
+</section>
+
+<section class='blue'>
+    <div class="container">
+        <div class='title'>      
+            <p class='yellow-text'>SERVICES</p>
+            <h1 class='white-text'><B>What We Offer</B></h1>
+            <div class='row'>                        
+                <div class="col-6 col-md-3 one1 icon-item">
+                <div class='round'><i class="fas fa-mobile-alt"></i></div>
+                <h2 class='white-text'>Responsive</h2>
+                <p class='white-text'>Looks great at any screen size!</p>
+                </div>
+                <div class="col-6 col-md-3 two2 icon-item">
+                <div class='round'><i class="far fa-edit"></i></div>
+                <h2 class='white-text'>Redesigned</h2>
+                <p class='white-text'>Freshly redesignet for Bootstrap 4.</p>
+                </div>                   
+                <div class="col-6 col-md-3 three3 icon-item">
+                <div class='round'><i class="far fa-thumbs-up"></i></div>
+                <h2 class='white-text't>Favorited</h2>
+                <p class='white-text'>Milion of users <i class="fas fa-heart"></i> Start Bootstrap!</p>
+                </div>
+                <div class="col-6 col-md-3 four4 icon-item">
+                <div class='round'><i class="far fa-question-circle"></i></div>
+                <h2 class='white-text'>Question</h2>
+                <p class='white-text'>I mustache you are question...</p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</section>
+
+<section class='yellow2'>
+    <div class="container-fluid flex">
+        <div class='title'>
+            <h1><B>Welcome to your next  website!</B></h1>           
+            <button class='button btn-blue'>Download Now!</button>
+        </div>
+    </div>
+</section>
+
+<section class='white'>
+    <div class="container">
+        <div class='title'>      
+            <p class='yellow-text'>PORTFOLIO</p>
+            <h1><B>Recent Project</B></h1>
+        </div>
+        <div class='row'>                        
+            <div class="col-12 col-md-6 one portfolio-item">
+                <a class="text-link">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas luctus non eros auctor fringilla. Donec semper at nunc eu viverra. Quisque sollicitudin scelerisque orci in accumsan.</a>
+                <a class="port-link">STATIONARY</a></div>
+            <div class="col-12 col-md-6 two portfolio-item">
+                <a class="text-link">Nam nisi metus, vulputate a tellus sit amet, laoreet scelerisque tellus.</a>
+                <a class="port-link">ICE CREAM</a></div>              
+            <div class="col-12 col-md-6 three portfolio-item">
+                <a class="text-link">Suspendisse convallis vel risus et egestas. Duis aliquet massa finibus justo rutrum finibus.</a>
+                <a class="port-link">STRAWBERIES</a></div>
+            <div class="col-12 col-md-6 four portfolio-item">
+                <a class="text-link">In porta ante erat. Maecenas non placerat justo. Vestibulum lacus ex, sagittis aliquam massa in, dignissim fringilla ligula.</a>
+                <a class="port-link">WORKSPACE</a></div>
+        </div>
+    </div>
+</section>
+
+<section class='blue'>
+    <div class="container-fluid">
+    <div class='title'>           
+            <h2 class='white-text'>The buttons below are impossible to resist...</h2>
+            <button class='button'>Click Me!</button>
+            <button class='button btn-grey'>Look at Me!</button>
+        </div>
+    </div>
+</section>
+
+<section class='grey'>
+    <div class="container-fluid">
+    </div>
+</section>
+
+<section class='white'>
+    <div class="container-fluid">
+        <div class='title'>           
+            <p2>Copyright (c) Your Website 2018</22>
+    </div>
+    </div>
+</section>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+
+<script src="script.js"></script>
+</body>
+</html>
